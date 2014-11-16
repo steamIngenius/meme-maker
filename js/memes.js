66923e015fffea18968365c1f36b171ca9973175
Micah Hawkins <micahbhawkins@gmail.com>
2014-11-16 14:31:03 -0800

    Finished first working version of meme generator 	To test, type 'python -m SimmpleHTTPServer'

--- js/memes.js
+++ js/memes.js
@@ -1,31 +1,86 @@
-var c = document.querySelector("#c");
-var ctx = c.getContext("2d");
+function textChangeListener(evt) {
+	// grab the id and value of what changed
+	var id = evt.target.id;
+	var text = evt.target.value;
 
-// ctx.fillRect(100, 100, 100, 100);
-// ctx.strokeRect(50, 50, 50, 50);
+	// update the text accordingly
+	if (id == "topLineText") {
+		window.topLineText = text;
+	} else {
+		window.bottomLineText = text;
+	}
 
-ctx.fillStyle = "black";
-ctx.strokeStyle = "gray";
+	// refresh meme contents with changes
+	redrawMeme(window.imageSrc, window.topLineText, window.bottomLineText);
+}
 
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke();
-ctx.closePath();
+function redrawMeme(image, topLine, bottomLine) {
+	// get 2d context of the canvas
+	var canvas = document.querySelector('canvas');
+	var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.moveTo(75, 75);
-ctx.lineTo(125, 75);
-ctx.lineTo(125, 125);
-ctx.fill();
+	// clear canvas
+	canvas.width = canvas.width;
 
-ctx.fillStyle = "white";
-ctx.strokeStyle = "black";
-ctx.textAlign = "center";
-ctx.font = "bold 36px Impact";
-ctx.lineWidth = 2;
-ctx.fillText("canvas memes!".toUpperCase(), c.width / 2, 40);
-ctx.strokeText("canvas memes!".toUpperCase(), c.width / 2, 40);
+	// draw image
+	if (image != null) {
+		ctx.drawImage(image, 0, 0);
+	}
+
+	// set up text
+	ctx.font = "bold 36px Impact";
+	ctx.lineWidth = 2;
+	ctx.fillStyle = "white";
+	ctx.strokeStyle = "black";
+	ctx.textAlign = "center";
+
+	// draw top text
+	if (topLine != null) {
+		ctx.fillText(topLine.toUpperCase(), canvas.width / 2, 40);	
+		ctx.strokeText(topLine.toUpperCase(), canvas.width / 2, 40);
+	}
+
+	// draw bottom text
+	if (bottomLine != null) {
+		ctx.fillText(bottomLine.toUpperCase(), canvas.width / 2, canvas.height - 60);	
+		ctx.strokeText(bottomLine.toUpperCase(), canvas.width / 2, canvas.height - 60);
+	}
+	
+}
+
+function saveFile() {
+	// convert the canvas to an image and open in a new tab/window
+	window.open(document.querySelector('canvas').toDataURL());
+}
+
+function handleFileSelect(evt) {
+	var file = evt.target.files[0];
+	var reader = new FileReader();
+
+	reader.onload = function(fileObject) {
+		var data = fileObject.target.result;
+
+		var image = new Image();
+		image.onload = function() {
+			window.imageSrc = this;
+			redrawMeme(window.imageSrc, window.topLineText, window.bottomLineText);
+		}
+
+		image.src = data;
+		console.log(fileObject.target.result);
+	}
+
+	reader.readAsDataURL(file);
+}
+
+window.topLineText = "type your meme -->";
+window.bottomLineText = "";
+
+var input1 = document.getElementById('topLineText');
+var input2 = document.getElementById('bottomLineText');
+
+input1.oninput = textChangeListener;
+input2.oninput = textChangeListener;
+
+document.getElementById('file').addEventListener('change', handleFileSelect, false);
+document.getElementById('saveButton').addEventListener('click', saveFile, false);
